cb79f736208f66d4449e96e964c600e7accd4155
Dexter8777 <mosesdexter2019@gmail.com>
2024-02-18 12:59:39 +0000

    Toggle off the error class on both password inputs initially

---
+++ app.js
@@ -0,0 +1,9 @@
+confirmPassword = document.getElementById('confirmPassword');
+password = document.getElementById('password');
+
+password.classList.toggle('error');
+confirmPassword.classList.toggle('error');
+
+// function matchPassword {
+
+// }
